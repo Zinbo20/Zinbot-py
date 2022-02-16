feb32ec9b6eeb10507c6136e4fb0634d2f8ffedc
Caique Leandro Tessaroto <87505774+Zinbo20@users.noreply.github.com>
2022-02-16 03:50:23 +0000

    cokiefile

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ canal_voice2 = 0
 YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
 
 FFMPEG_OPTIONS = {
+    'cookiefile': 'cookies.txt',
     'before_options':
     '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
     'options': '-vn'
