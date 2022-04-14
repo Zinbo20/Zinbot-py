20f293772725e3ca853149c889141fa4b8e37465
Caique Leandro Tessaroto <87505774+Zinbo20@users.noreply.github.com>
2022-04-14 01:02:38 +0000

    voice

--- functions/timeout.py
+++ functions/timeout.py
@@ -19,10 +19,9 @@ async def timeout(message, voice,client2):
     while True:
       await asyncio.sleep(30)
       m += 1
+      voice2 = discord.utils.get(client.voice_clients, guild=message.author.guild)
       if m == 10*2 or voice.is_playing() or voice.is_paused() or voice2 == None:
         break
-    
-    voice2 = discord.utils.get(client.voice_clients, guild=message.author.guild)
     
     if voice.is_playing() or voice.is_paused() or voice2 == None:
       break
